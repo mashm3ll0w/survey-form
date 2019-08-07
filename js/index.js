e0f1617d80cff2e5fcd81768fe500fa8d11293fb
Charles Swaleh <charlswall@live.com>
2019-08-07 21:24:25 +0300

    get the js working properly

--- js/index.js
+++ js/index.js
@@ -1,12 +1,15 @@
-var name = $("#name").val();
-var food = $("#food").val();
-var music = $("#music").val();
-var gender = $("input:radio[name=gender]:checked").val();
-var opSystem = $("#os").val();
-
 $(document).ready(function() {
 	$("form#survey").submit(function(event) {
 		event.preventDefault();
-		alert("Thank you, " + name + " for your time. Next time, you will find " + food + " here waiting for you and " + music + " playing in the background. You are special because not many " + gender + "s can survive a minute using " + opSystem + " OS!!");
+
+		var name = $("input#name").val();
+		var food = $("input#food").val();
+		var music = $("input#music").val();
+		var gender = $("input[name='gender']:checked").val();
+		var opSystem = $("#operatingSystem").val();
+
+		alert("Thank you, " + name + ". Next time, you will find " + food + " here for you and " + music + " playing in the background. You are special because not many " + gender + "s can survive a minute using " + opSystem + " OS!!");
+
+		$("div.catch-form").text("Thank you, " + name + ". Next time, you will find " + food + " here for you and " + music + " playing in the background. You are special because not many " + gender + "s can survive a minute using " + opSystem + " OS!!");
 	});
 });
